e9b5625ba6561c183c33b08500c20577407ca8ed
Magnum Fonseca <magnumfonseca@Magnums-MacBook-Pro.local>
2020-05-20 13:53:52 -0300

    processing transactions

--- bin/account_movement.rb
+++ bin/account_movement.rb
@@ -7,8 +7,20 @@ def process_acounts_file(data_file)
   csv_accounts.to_h { |k, v| [k, v.to_i] }
 end
 
-def process_transactions
+def process_transactions(accounts, data_file)
+  transactions = read_csv_file(data_file)
 
+  transactions.each do |account, value|
+    accounts[account] = calculate_transaction(accounts[account], value)
+  end
+
+  accounts
+end
+
+def calculate_transaction(balance, value)
+  balance += value
+  balance -= 300 if (balance < 0 && value < 0)
+  balance
 end
 
 def read_csv_file(data_file)
@@ -20,6 +32,6 @@ def read_csv_file(data_file)
 end
 
 if $0 == __FILE__
-  raise ArgumentError, "Usage: #{$0} <filename>" unless ARGV.length == 1
+  raise ArgumentError, "Usage: #{$0} <filename>" unless ARGV.length == 2
   #process(ARGV[0])
 end
--- spec/bin/account_movement_spec.rb
+++ spec/bin/account_movement_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'account_movement' do
   describe '#read_csv_file' do
     let(:csv_account_file) { 'spec/fixtures/contas.csv' }
     let(:expected) { [[123,13052], [531,89000], [2314,123400]] }
+
     it{ expect(read_csv_file(csv_account_file)).to eq expected }
 
     context 'invalid file'do
@@ -17,6 +18,41 @@ RSpec.describe 'account_movement' do
   describe '#process_acounts_file' do
     let(:csv_account_file) { 'spec/fixtures/contas.csv' }
     let(:expected) { {123 => 13052, 531 => 89000, 2314 => 123400} }
+
     it{ expect(process_acounts_file(csv_account_file)).to eq expected }
+  end
+
+  describe '#process_transactions' do
+    let(:accounts) { {123 => 13052, 531 => 89000, 2314 => 123400} }
+    let(:csv_transactions_file) { 'spec/fixtures/transacoes.csv' }
+    let(:expected) { {123 => -2748, 531 => 21400, 2314 => 18100 } }
+
+    it{ expect(process_transactions(accounts, csv_transactions_file)).to eq expected }
+
+    context 'account with negative balance' do
+      let(:accounts) { {123 => -1000} }
+
+      context 'debit' do
+        let(:expected) { {123 => -2300} }
+
+        it 'applies 300 fee' do
+          expect(CSV).to receive(:read).
+            with(csv_transactions_file, {:converters=>:numeric, :headers=>false}) {[[123,-1000]]}
+
+          expect(process_transactions(accounts, csv_transactions_file)).to eq expected
+        end
+      end
+
+      context 'credit' do
+        let(:expected) { {123 => -100} }
+
+        it 'applies 300 fee' do
+          expect(CSV).to receive(:read).
+            with(csv_transactions_file, {:converters=>:numeric, :headers=>false}) {[[123,900]]}
+
+          expect(process_transactions(accounts, csv_transactions_file)).to eq expected
+        end
+      end
+    end
   end
 end
