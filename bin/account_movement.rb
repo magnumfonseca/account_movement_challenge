ce7cbac4075009cdabed13eda0e8403dd485b82c
Magnum Fonseca <magnumfonseca@Magnums-MacBook-Pro.local>
2020-05-20 12:38:38 -0300

    processing accounts file

--- bin/account_movement.rb
+++ bin/account_movement.rb
@@ -1,5 +1,25 @@
-if ARGV.length != 2
-  puts 'We need exactly two arguments (acounts file, transactions file)'
-  exit
+#!/usr/bin/env ruby
+
+require 'csv'
+
+def process_acounts_file(data_file)
+  csv_accounts =  read_csv_file(data_file)
+  csv_accounts.to_h { |k, v| [k, v.to_i] }
 end
 
+def process_transactions
+
+end
+
+def read_csv_file(data_file)
+  begin
+    CSV.read(data_file, headers: false, converters: :numeric).reject(&:empty?)
+  rescue => ex
+    raise "Error reading #{data_file}: #{ex}"
+  end
+end
+
+if $0 == __FILE__
+  raise ArgumentError, "Usage: #{$0} <filename>" unless ARGV.length == 1
+  #process(ARGV[0])
+end
--- spec/bin/account_movement_spec.rb
+++ spec/bin/account_movement_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
-#require_relative '../../bin/account_movement'
+require_relative '../../bin/account_movement'
 
-RSpec.describe 'validate arguments' do
-  it{ expect(true).to be true }
+RSpec.describe 'account_movement' do
+  describe '#read_csv_file' do
+    let(:csv_account_file) { 'spec/fixtures/contas.csv' }
+    let(:expected) { [[123,13052], [531,89000], [2314,123400]] }
+    it{ expect(read_csv_file(csv_account_file)).to eq expected }
+
+    context 'invalid file'do
+      let(:csv_invalid_file) { 'spec/fixtures/invalid.csv' }
+
+      it { expect{ read_csv_file(csv_invalid_file) }.to raise_error(RuntimeError, /Error reading/) }
+    end
+  end
+
+  describe '#process_acounts_file' do
+    let(:csv_account_file) { 'spec/fixtures/contas.csv' }
+    let(:expected) { {123 => 13052, 531 => 89000, 2314 => 123400} }
+    it{ expect(process_acounts_file(csv_account_file)).to eq expected }
+  end
 end
